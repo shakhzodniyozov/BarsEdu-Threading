b584ee27746aef7302f1dc5502a72c1570628ecc
Shakhzod Niyozov <shakhzodniyozov@yandex.ru>
2022-04-10 23:35:58 +0300

    Now using ThreadPool instead of Thread

--- Program.cs
+++ Program.cs
@@ -20,28 +20,27 @@
             arguments.Add(argument!);
     }
 
-    Thread thread = new Thread(() =>
+    ThreadPool.QueueUserWorkItem(state =>
     {
+        var messageId = Guid.NewGuid();
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"--> Было отправлено сообщение '{message}'. Присвоен идентификатор {messageId}");
+        Console.ForegroundColor = ConsoleColor.Gray;
+
         try
         {
             var response = new DummyRequestHandler().HandleRequest(message!, arguments.ToArray());
 
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"<-- Сообщение с идентификатором {Thread.CurrentThread.ManagedThreadId} получил ответ - {response}.");
+            Console.WriteLine($"<-- Сообщение с идентификатором {messageId} получил ответ - {response}.");
             Console.ForegroundColor = ConsoleColor.Gray;
         }
         catch (Exception e)
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Сообщение с идентификатором {Thread.CurrentThread.ManagedThreadId} упало. {e.Message}");
+            Console.WriteLine($"Сообщение с идентификатором {messageId} упало. {e.Message}");
             Console.ForegroundColor = ConsoleColor.Gray;
         }
     });
-
-    thread.IsBackground = true;
-    thread.Start();
-
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine($"--> Было отправлено сообщение '{message}'. Присвоен идентификатор {thread.ManagedThreadId}");
-    Console.ForegroundColor = ConsoleColor.Gray;
 }
